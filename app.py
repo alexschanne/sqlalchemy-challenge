cfe6a72b8e9b1b5c920d5696b0059eb80600467d
Alex Schanne <alex.schanne@gmail.com>
2020-08-25 19:13:32 -0400

    adding/updating app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@
 import numpy as np 
 import datetime as dt 
 import sqlalchemy 
-from sqlalchemy.ext.automap import automap
+from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
 #Database Setup
-engine = create_engine('sqlite://hawaii.sqlite')
+engine = create_engine('sqlite:///Resources/hawaii.sqlite')
 Base = automap_base()
 Base.prepare(engine, reflect = True)
 
@@ -35,7 +35,17 @@ def welcome():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
-    queryresult = session.query(precipitation)
+    queryresult = session.query(measurement.date, measurement.prcp).all()
+    session.close()
+
+    precipitation = []
+    for date, prcp in queryresult: 
+        precip_dict = {}
+        precip_dict["Date"] = date
+        precip_dict["Precipitation"] = prcp
+        precipitation.append(precip_dict)
+
+    return jsonify(precipitation)
 
 @app.route("/api/v1.0/stations")
 def stations():
@@ -55,11 +65,27 @@ def stations():
 
     return jsonify(stations)
 
-@app.route("api/v1.0/tobs")
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    latest = session.query(measurement.date).ordery_by(measurement.date.desc()).first()[0]
+    lastdate = dt.datetime.strptime(latest, '%Y-%m-%d')
+    querydt = dt.date(latest.year - 1, latest.month, latest.day)
+    
+    queryresult = session.query(measurement.date, measurement.tobs).filter(measurement.date >= querydt).all()
+    session.close()
 
+    tobsall = []
+    for date, tobs in queryresult:
+        tobs_dict = {}
+        tobs_dict["Date"] = date
+        tobs_dict["Tobs"] = tobs
+        tobsall.append(tobs_dict)
+
+    return jsonify(tobsall)
 
 @app.route("/api/v1.0/<start>")
-def get_t_start(start):
+def get_start(start):
     session = Session(engine)
     queryresult = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).filter(measurement.date >= start).all()
     session.close()
@@ -75,7 +101,7 @@ def get_t_start(start):
     return jsonify(tobsall)
 
 @app.route('/api/v1.0/<start>/<stop>')
-def get_t_start_stop(start,stop):
+def start_stop(start,stop):
     session = Session(engine)
     queryresult = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).filter(measurement.date >= start).filter(measurement.date <= stop).all()
     session.close()
@@ -86,9 +112,9 @@ def get_t_start_stop(start,stop):
         tobs_dict['Min'] = min
         tobs_dict['Avg'] = avg
         tobs_dict['Max'] = max
-        tobsall. append(tobs_dict)
+        tobsall.append(tobs_dict)
 
     return jsonify(tobsall)
 
-
-    
+if __name__ == '__main__':
+    app.run(debug = True) 
